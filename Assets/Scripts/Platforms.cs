41493912e16790ec400e22b8a3c0673b592b9d28
Rocky5oh <stephenrock206@gmail.com>
2023-10-31 00:44:39 +0000

    Worked on shooting in the direction of mousex mouse y

--- Assets/Scripts/Platforms.cs
+++ Assets/Scripts/Platforms.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletController : MonoBehaviour
+public class Platforms : MonoBehaviour
 {
-
-    private float speed = 150.0f;
-
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Movement variables
     public float horizontalInput;
     public float speed = 50.0f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+
     public float XRange = 73;
 
     public GameObject projectilePrefab;
 
-    private Rigidbody playerRb;
+    //Jumping variables
+    private Rigidbody2D playerRb;
     public float jumpForce;
     public float gravityModifier;
 
     public bool isOnGround = true;
 
+    //Flipping player variable
     public bool facingRight = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
+        playerRb = GetComponent<Rigidbody2D>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -30,17 +41,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Player movement
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        movement.x = Input.GetAxis("Horizontal");
         //Player remain inbound
         if(transform.position.x < -73)
         {
-            transform.position = new Vector3(-XRange, transform.position.y, transform.position.z);
+            transform.position = new Vector2(-XRange, transform.position.y);
         }
 
         if(transform.position.x > 73)
         {
-            transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
+            transform.position = new Vector2(XRange, transform.position.y);
         }
         //Launch bullet from player
         if(Input.GetKey(KeyCode.Space))
@@ -50,30 +61,24 @@ public class PlayerController : MonoBehaviour
         //Player Jumping
         if(Input.GetKeyDown(KeyCode.UpArrow) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
-        //Flip Player
-        if (horizontalInput > 0 && !facingRight)
-        {
-            Flip();
-        }
-        if (horizontalInput < 0 && facingRight)
-        {
-            Flip();
-        }
+        cam.ScreenToWorldPoint(Input.mousePosition);
+
     }
     private void OnCollisionEnter(Collision collision)
     {
         isOnGround = true;
     }
 
-    void Flip()
+    private void FixedUpdate()
     {
-        Vector3 currentScale = gameObject.transform.localScale;
-        currentScale.x += -3;
-        gameObject.transform.localScale = currentScale;
+        rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * movement);
 
-        facingRight = !facingRight;
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
+
 }
